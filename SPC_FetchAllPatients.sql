a46336e3afeed96da302f2e7b79e57864470c11a
Nagapandian N <pq.nagapandian@outlook.com>
2020-03-19 10:55:13 +0530

    new stored procedures

--- SPC_FetchAllPatients.sql
+++ SPC_FetchAllPatients.sql
@@ -1,9 +1,6 @@
-
-alter Procedure [dbo].[SPC_FetchAllPatients] (@GID int)
+alter Procedure [dbo].[SPC_FetchAllPatients]
 As
 Begin
-	if @GID = 0
-	Begin
 	Select 
 		[ID]
 		,[GID]
@@ -12,18 +9,4 @@ Begin
 		,[CITY] 
 	From [dbo].[TBL_PATIENT] 
 	Order by [GID]
-	End
-	else
-	if @GID > 0
-	Begin
-	Select 
-		[ID]
-		,[GID]
-		,[FIRSTNAME] 
-		,[LASTNAME] 
-		,[CITY] 
-	From [dbo].[TBL_PATIENT] where GID = @GID
-	End
 End
-
-GO
